5a7e062c90d6b7e802adc1a9df9bd85b32eac9d6
nikitamavrodiy <mna1998@mail.ru>
2022-12-10 23:35:05 +0300

    completed...

--- src/main/java/com/skypro/exam/service/ExamServiceImpl.java
+++ src/main/java/com/skypro/exam/service/ExamServiceImpl.java
@@ -6,25 +6,30 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class ExamServiceImpl implements ExamService {
-    private final QuestionService questionService;
+    private final List<QuestionService> questionServices;
 
-    public ExamServiceImpl(QuestionService questionService) {
-        this.questionService = questionService;
+    public ExamServiceImpl(List<QuestionService> questionServices) {
+        this.questionServices = questionServices;
     }
 
     @Override
     public Collection<Question> getQuestions(int amount) {
         Set<Question> questions = new HashSet<>();
-        if (questionService.getAll().size() < amount) {
-            throw new UnSufficientQuestionsException();
-        }
         while (questions.size() < amount) {
-            questions.add(questionService.getRandomQuestion());
+            questions.add(questionServices
+                    .get(ThreadLocalRandom.current().nextInt(0, size()))
+                    .getRandomQuestion());
         }
         return questions;
+    }
+
+    public int size() {
+        return questionServices.size();
     }
 }
--- src/test/java/com/skypro/exam/service/ExamServiceTest.java
+++ src/test/java/com/skypro/exam/service/ExamServiceTest.java
@@ -8,6 +8,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
